6cec495c9af0e3d8a3c0e13a348c8bbf9860d6cb
Eliangangs <eliangangs@gmail.com>
2025-12-02 18:14:58 +0200

    feat: clear

--- OOPLab3Tpl/OOPLab3Tpl.cpp
+++ OOPLab3Tpl/OOPLab3Tpl.cpp
@@ -1,24 +1,18 @@
-﻿// OOPLab3Tpl.cpp : Цей файл містить є шаблоном для 
-// виконання лаб. роботи №3. 
-// Він містинь функцію "main" з якої починається та закінчується виконання програми.
-//
-
-#include <iostream>
-
-// Ваші файли загловки 
-//
+﻿#include <iostream>
 #include "Lab3Example.h"
+using namespace std;
+
 int main()
 {
-    std::cout << " Lab #3  !\n";
+    cout << " Lab #3  !\n";
+
 //  Код виконання завдань
 //  Головне меню завдань
 //  Функції та класи можуть знаходитись в інших файлах проекту
 
     int conv = 1;
     if (conv == 1) conv = mainExample1();
-    if (conv == 1) conv = mainExample3(); 
-    if (conv == 1) conv = mainExample4();
-
+    if (conv == 1) conv = mainExample2(); 
+    if (conv == 1) conv = mainExample3();
 }
 
