b443dc8411ca16e6789098c1abc66e691cfa8132
Eliangangs <eliangangs@gmail.com>
2025-12-02 18:38:45 +0200

    feat: add MenuTask and switch

--- OOPLab3Tpl/OOPLab3Tpl.cpp
+++ OOPLab3Tpl/OOPLab3Tpl.cpp
@@ -6,13 +6,48 @@ int main()
 {
     cout << " Lab #3 (14v)  !\n";
 
-//  Код виконання завдань
-//  Головне меню завдань
-//  Функції та класи можуть знаходитись в інших файлах проекту
+    int conv;
+        while (true)
+    {
+        MenuTask();
+        cout << "Введіть номер завдання: ";
+        cin >> conv;
 
-    int conv = 1;
-    if (conv == 1) conv = main1();
-    if (conv == 1) conv = main2(); 
-    if (conv == 1) conv = main3();
+        switch (conv)
+        {
+        case 1:
+            main1();
+            break;
+
+        case 2:
+            main2();
+            break;
+
+        case 3:
+            main3();
+            break;
+
+        case 0:
+            cout << "Вихід із програми..." << endl;
+            return 0;
+
+        default:
+            cout << "Невірний вибір! Спробуйте ще раз." << endl;
+            break;
+        }
+
+        cout << "\nНатисніть Enter, щоб продовжити...\n";
+        cin.ignore();
+        cin.get();
+    }
+
+    return 0;
 }
 
+void MenuTask() {
+    cout << "===== Виберіть завдання =====\n";
+    cout << " 1. Task 1 — Parallelogram\n";
+    cout << " 2. Task 2 — Vector\n";
+    cout << " 3. Task 3 — Matrix\n";
+    cout << " 0. Exit\n";
+}
